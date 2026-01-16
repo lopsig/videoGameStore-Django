904369e3080d90765f84f70ec4b3eb5da68765b6
lopsig <jonathan.ls.trabajo@gmail.com>
2026-01-15 23:55:47 -0500

    catalog, pagination

--- videoGameStore/catalogo/models.py
+++ videoGameStore/catalogo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    platform = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, default="logo.jpeg")
+
+    def __str__(self):
+        return self.name
--- videoGameStore/catalogo/views.py
+++ videoGameStore/catalogo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from catalogo.models import Game
 
-# Create your views here.
+def list_games(request):
+    games = Game.objects.all().order_by('id')
+    paginator = Paginator(games, 3)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    context_catalog_games = {'list_games': page_obj}
+    return render(request, 'catalogo/list_games.html',
+                  context_catalog_games)
